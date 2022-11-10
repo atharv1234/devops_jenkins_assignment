2b7c40e0fa4257024bd403f406f81e62bf8fcb38
atharv1234 <atharvterwadkar@gmail.com>
2022-11-10 18:42:02 +0530

    change1

--- addition.c
+++ addition.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, sum;
+    int change1, number2, sum;
     
-    number1=1;number2=2;    
+    change1=1;number2=2;    
     // calculating sum
-    sum = number1 + number2;      
+    sum = change1 + number2;      
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d", change1, number2, sum);
     return 0;
 }
